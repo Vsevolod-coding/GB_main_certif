325279afc1084569e26b5398e923d5478300b1b1
Seva <vsevolodharlamenko@gmail.com>
2024-04-13 19:33:03 +0400

    added comments to code

--- Program.cs
+++ Program.cs
@@ -3,14 +3,20 @@
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
 
+// Ввод исходного массива
 string [] your_array = {"Vsevolod","Car","!0_","Joke","XD","27364","Хай","No","Yes","Com puter"};
 
+// Формирование нового массива строк
 string[] newArray = FilterArray(your_array);
 
+// Вывод результата
+Console.WriteLine("Результат:");
 Console.WriteLine(string.Join(", ", newArray));
 
+// Метод для фильтрации массива строк
 static string[] FilterArray(string[] Array)
 {
+    // Подсчет количества строк, удовлетворяющих условию
     int count = 0;
     foreach (string str in Array)
     {
@@ -19,8 +25,10 @@ static string[] FilterArray(string[] Array)
             count++;
         }
     }
+    // Создание нового массива нужного размера
     string[] FinalArray = new string[count];
 
+    // Заполнение нового массива строками
     int index = 0;
     foreach (string str in Array)
     {
