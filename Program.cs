eadb7a299c0fcc436ef27799564ccb4d7b26cebd
Seva <vsevolodharlamenko@gmail.com>
2024-04-13 18:28:13 +0400

    Added array

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+string [] your_array = {};
